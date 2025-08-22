75d017bfe594fd89cf99da7d4f47ea9cceae92ee
alexnezumi <alexsilva.viana2006@gmail.com>
2025-08-22 08:51:25 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,88 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // lista 1
+        // exercicio - Faça um programa que leia um número inteiro e o imprima;
+
+       /*
+       int x = 5;
+        System.out.println(x);
+       */
+        // exercicio - Faça um programa que leia um número real e o imprima;
+
+       /*
+        double x = 12.45;
+        System.out.println(x);
+       */
+
+        // exercicio - Peça ao usuário para digitar três valores inteiros e imprima a soma deles;
+
+     /*
+     Scanner grava = new Scanner(System.in);
+     int num1,num2,num3,soma;
+
+     System.out.println("Insira o primeiro número");
+     num1 = grava.nextInt();
+
+     System.out.println("insira o segundo número");
+     num2 = grava.nextInt();
+
+     System.out.println("Insira o terceiro número");
+     num3 = grava.nextInt();
+
+     soma = num1+num2+num3;
+
+     System.out.println("A soma dos números " + num1 + ", " + num2 + " e " + num3 + " é " + soma );
+     */
+
+        // exercicio - Peça ao usuário inserir a temperatura em °C e imprima a temperatura em °F
+
+     /*
+        Scanner grava = new Scanner(System.in);
+        double c, f;
+
+        System.out.println("Insira a temperatura em graus que você deseja converter");
+        c= grava.nextDouble();
+
+        f = c * (9.0 / 5.0) + 32;
+
+        System.out.println("O resultado em fahrenheit da temperatura é " + f + " F°");
+     */
+
+        // exercicio - Faça para converter a velocidade de km/h para m/s.
+
+     /*
+        Scanner grava = new Scanner(System.in);
+
+        System.out.println("Digite a velocidade em km/h: ");
+
+        double kmh = grava.nextDouble();
+
+        double ms = kmh / 3.6;
+
+        System.out.printf("Velocidade em m/s: %.2f\n", ms);
+
+      */
+
+        // exercicio - Peça ao usuário inserir um número inteiro e imprima o seu antecessor;
+
+      /*
+       Scanner grava = new Scanner(System.in);
+
+       int x;
+
+       System.out.println("Insira um numero inteiro para que eu te mostre seu antecessor");
+       x = grava.nextInt();
+       x = x - 1;
+
+       System.out.println("O antecessor do número digitado é " + x);
+
+       */
+
+        // exercicio - Peça ao usuário inserir seu ano de nascimento e imprima sua idade;
+
+
+
+    }
+}
